fc22942503246e4524b7da73df5e94f31ef314c4
Anton Shulga <shandm85@gmail.com>
2018-10-26 14:47:06 +0300

    Add shortcode on MyAccount page

--- templates/template-my-account.php
+++ templates/template-my-account.php
@@ -23,10 +23,15 @@ $options = get_option('TheScholarshipHub');
                                                         $user = wp_get_current_user();
                                                         $arr_scholarship_ids = array();
                                                         $is_email_premium_tab = false;
-                                                        if ($user != false && defined( 'RCP_PLUGIN_DIR' ) && rcp_get_subscription_id() >= 2) {
+                                                        $is_member_benefits_tab = false;
+                                                        
+                                                        if ($user != false && defined( 'RCP_PLUGIN_DIR' ) && (rcp_get_subscription_id() >= 2 || current_user_can('administrator'))) {
                                                             $arr_scholarship_ids = get_user_meta($user->ID, '_scholarship_selected', true);
                                                             $arr_scholarship_ids = $arr_scholarship_ids != false ? $arr_scholarship_ids : array();
-                                                            $is_email_premium_tab = rcp_get_subscription_id() == 3 ? true : false;
+                                                            
+                                                            $is_email_premium_tab = (rcp_get_subscription_id() == 3 || current_user_can('administrator')) ? true : false;
+                                                            
+                                                            $is_member_benefits_tab = true;
                                                         }
                                                         ?>
                                                         <ul class="nav nav-tabs">
@@ -35,10 +40,13 @@ $options = get_option('TheScholarshipHub');
                                                                 <li role="presentation" class="tab3" ><a data-tab="3" href="#">Billing</a></li>
                                                                 <?php
                                                                 if (!empty($arr_scholarship_ids)) { ?>
-                                                                    <li role="presentation" class="tab4" ><a data-tab="4" href="#">Favorited scholarship</a></li>
+                                                                    <li role="presentation" class="tab4" ><a data-tab="4" href="#">Favorited scholarships</a></li>
                                                                 <?php }
-                                                                if ($is_email_premium_tab || current_user_can('administrator')) { ?>
-                                                                    <li role="presentation" class="tab5" ><a data-tab="5" href="#">Email alerts</a></li>                                                                    
+                                                                if ($is_member_benefits_tab) { ?>
+                                                                    <li role="presentation" class="tab5" ><a data-tab="5" href="#">Member Benefits</a></li>                                                                    
+                                                                <?php }
+                                                                if ($is_email_premium_tab) { ?>
+                                                                    <li role="presentation" class="tab6" ><a data-tab="6" href="#">Subscription alerts</a></li>                                                                    
                                                                 <?php } ?>
                                                         </ul>
                                                         <div class="tab tab1 active"><?php echo do_shortcode('[rcp_profile_editor]');?></div>
@@ -48,8 +56,11 @@ $options = get_option('TheScholarshipHub');
                                                         if (!empty($arr_scholarship_ids)) { ?>
                                                             <div class="tab tab4"><?php echo do_shortcode('[tsh_favorited_scholarship ids="'. implode(',', $arr_scholarship_ids) .'"]');?></div>
                                                         <?php }
-                                                        if ($is_email_premium_tab || current_user_can('administrator')) { ?>
-                                                            <div class="tab tab5"><?php echo do_shortcode('[tsh_scholarship_email_options]');?></div>
+                                                        if ($is_member_benefits_tab) { ?>
+                                                            <div class="tab tab5"><?php echo do_shortcode('[tsh_member_benefits]');?></div>
+                                                        <?php }
+                                                        if ($is_email_premium_tab) { ?>
+                                                            <div class="tab tab6"><?php echo do_shortcode('[tsh_scholarship_email_options]');?></div>
                                                         <?php } ?>
                                                 </div>
                                         </div><!-- .entry-content -->
